a75bac25ec7d25122f671c120f67a278cb15c7d6
AnthonySinitsa <itonyemail@gmail.com>
2023-06-30 19:02:04 +0000

    usedCommand

--- playground/tree/binary_tree.py
+++ playground/tree/binary_tree.py
@@ -1,7 +1,19 @@
-class BinaryTree:
+class Node:
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
 
-  def pre_order(self):
-    pass
+class BinaryTree:
+  def pre_order(self, values=[]):
+    def walk(input_node, value_list):
+      if not input_node:
+        return
+      value_list.append(input_node.value)
+      walk(input_node.left, value_list)
+      walk(input_node.right, value_list)
+    walk(self.root, values)
+    return
 
   def in_order(self):
     pass
