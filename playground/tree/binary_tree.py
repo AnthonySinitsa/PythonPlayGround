d5ed3a8204079ee29da37077445a8f3ac42f7f0e
AnthonySinitsa <itonyemail@gmail.com>
2023-06-30 18:29:54 +0000

    main

---
+++ playground/tree/binary_tree.py
@@ -0,0 +1,17 @@
+class BinaryTree:
+
+  def pre_order(self):
+    pass
+
+  def in_order(self):
+    pass
+
+  def post_order(self):
+    pass
+
+class BinarySearchTree(BinaryTree):
+  def add(self, value):
+    pass
+
+  def contains(self, value):
+    pass
