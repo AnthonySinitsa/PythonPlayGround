6a1203b2b612e41a6614acd142ee0d05062b32a9
AnthonySinitsa <itonyemail@gmail.com>
2023-07-11 23:17:30 +0000

    usedCommand

--- chatgpt/chatgpt.py
+++ chatgpt/chatgpt.py
@@ -1,11 +1,24 @@
+# from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+# def generate_response(input_text):
+#     input_ids = tokenizer.encode(input_text, return_tensors="pt")
+#     response = model.generate(input_ids, max_length=100)[0]
+#     response_text = tokenizer.decode(response, skip_special_tokens=True)
+#     return response_text
+
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 
 def generate_response(input_text):
     input_ids = tokenizer.encode(input_text, return_tensors="pt")
-    response = model.generate(input_ids, max_length=100)[0]
+    attention_mask = torch.ones_like(input_ids)
+    pad_token_id = tokenizer.eos_token_id
+    response = model.generate(input_ids, attention_mask=attention_mask, pad_token_id=pad_token_id, max_length=100)[0]
     response_text = tokenizer.decode(response, skip_special_tokens=True)
     return response_text
-
