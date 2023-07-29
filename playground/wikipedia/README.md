292e56ad763238c87ef94637363ed1f7f64e6c7f
AnthonySinitsa <itonyemail@gmail.com>
2023-07-29 18:24:43 +0000

    command

---
+++ playground/wikipedia/README.md
@@ -0,0 +1,11 @@
+# Wikipedia
+
+## Info
+
+Wikipedia is a Python library that makes it easy to access and parse data from Wikipedia
+
+- <https://wikipedia.readthedocs.io/en/latest/code.html#api>
+
+- pip install wikipedia
+
+- python playground/wikipedia/wiki.py
--- playground/wikipedia/wiki.py
+++ playground/wikipedia/wiki.py
@@ -1,4 +1,60 @@
 import wikipedia
-ba = wikipedia.search("Barack")
-ba = wikipedia.summary("Barack", sentences=1)
-print(ba)
+import os
+
+def search(str):
+  os.system('clear')
+  print(wikipedia.search(str))
+  
+def summary(str, sentence):
+  os.system('clear')
+  print(wikipedia.summary(str, sentence)) # change sentence amount
+  
+def lang(str):
+  os.system('clear')
+  wikipedia.set_lang(str)
+  print(summary('computer', 10))
+  print()
+
+def page(str):
+  os.system('clear')
+  print(wikipedia.page(str))
+
+def content(str):
+  os.system('clear')
+  print(wikipedia.page(str).content)
+  
+def images(str):
+  os.system('clear')
+  print(wikipedia.page(str).images)
+  
+def categories(str):
+  os.system('clear')
+  print(wikipedia.page(str).categories)  
+
+def random(pages):
+  os.system('clear')
+  print(wikipedia.random(pages))
+  
+def html(str):
+  os.system('clear')
+  print(wikipedia.page(str).html())
+
+def geosearch(lat, lon, title=None, results=10, radius=1000):
+  os.system('clear')
+  print(wikipedia.geosearch(lat, lon, title, results, radius))
+
+if __name__ == '__main__':
+  search('Barack')
+  # summary('Barack', 10)
+  # lang('fr')
+  # lang('uk')
+  # lang('vi')
+  # lang('de')
+  # page('cheesecake')
+  # content('cheesecake')
+  # images('cheesecake')
+  # categories('cheesecake')
+  # random(pages=10)
+  # html('cheesecake')
+  # geosearch(47.6205, -122.3493, results=30, radius=1000)
+  # radius in meters, value must be between 10 and 10000
