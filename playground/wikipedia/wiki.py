6c8f53fdeca4e3005692650d5cbe8c28fac8efd3
AnthonySinitsa <itonyemail@gmail.com>
2023-08-01 17:25:44 +0000

    command

--- playground/wikipedia/wiki.py
+++ playground/wikipedia/wiki.py
@@ -40,14 +40,14 @@ def geosearch(lat, lon, title=None, results=10, radius=1000):
   print(wikipedia.geosearch(lat, lon, title, results, radius))
 
 if __name__ == '__main__':
-  search('Barack')
+  # search('Barack')
   # summary('Barack', 10)
   # lang('fr')
   # lang('de')
   # content('cheesecake')
   # images('cheesecake')
   # categories('cheesecake')
-  # random(pages=10)
+  # random(pages=50)
   # html('cheesecake')
-  # geosearch(47.6205, -122.3493, results=30, radius=1000)
+  geosearch(47.6205, -122.3493, results=30, radius=1000)
   # radius in meters, value must be between 10 and 10000
