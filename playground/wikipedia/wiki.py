1470818ba4545db083cf2e861514a492e699c465
AnthonySinitsa <itonyemail@gmail.com>
2023-08-01 16:48:13 +0000

    command

--- playground/wikipedia/wiki.py
+++ playground/wikipedia/wiki.py
@@ -3,37 +3,37 @@ import os
 
 def search(str):
   os.system('clear')
-  print(wikipedia.search(str))
+  print(wikipedia.search(str)) # shows different results using 'barack'
   
 def summary(str, sentence):
   os.system('clear')
-  print(wikipedia.summary(str, sentence)) # change sentence amount
+  print(wikipedia.summary(str, sentence)) # gives sentence summary
   
 def lang(str):
   os.system('clear')
   wikipedia.set_lang(str)
-  print(summary('computer', 10))
+  print(summary('computer', 10)) # gives summary in different languages
   print()
 
 def content(str):
   os.system('clear')
-  print(wikipedia.page(str).content)
+  print(wikipedia.page(str).content) # gives multiple definitions
   
 def images(str):
   os.system('clear')
-  print(wikipedia.page(str).images)
+  print(wikipedia.page(str).images) # images
   
 def categories(str):
   os.system('clear')
-  print(wikipedia.page(str).categories)  
+  print(wikipedia.page(str).categories) # shows 'related' categories
 
 def random(pages):
   os.system('clear')
-  print(wikipedia.random(pages))
+  print(wikipedia.random(pages)) # gives random topics
   
 def html(str):
   os.system('clear')
-  print(wikipedia.page(str).html())
+  print(wikipedia.page(str).html()) # gives html of said page
 
 def geosearch(lat, lon, title=None, results=10, radius=1000):
   os.system('clear')
