d35cfd9b3a8662af89f411cc666245b1d9dc8444
AnthonySinitsa <itonyemail@gmail.com>
2023-08-15 21:33:07 +0000

    command

---
+++ playground/saveFile/saveFile.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.indeed.com/jobs?q=python+developer&l=Seattle%2C+WA&from=searchOnHP"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36"
+}
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Prettify the HTML content
+prettified_content = soup.prettify()
+
+# Save the prettified HTML content to a text file
+with open('indeed_page_content_prettified.txt', 'w', encoding='utf-8') as file:
+    file.write(prettified_content)
+
+print("Prettified HTML content saved to 'indeed_page_content_prettified.txt'.")
+
+# print(soup)
+# print(tables)
+
+# df1 = pd.read_html(str(tables[0]))
+# df1 = df1[0]
+# print(df1)
+# df2 = pd.read_html(str(tables[1]))
+# df2 = df2[0]
+# print(df2)
+
