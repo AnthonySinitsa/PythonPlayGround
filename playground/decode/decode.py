f2c0f0c8d71cbe2165c0fb263d97807c6716b74d
AnthonySinitsa <itonyemail@gmail.com>
2024-05-08 00:09:49 +0000

    command

--- playground/decode/decode.py
+++ playground/decode/decode.py
@@ -14,17 +14,12 @@ def decode(message_file):
     decoded_message = ""
 
     for _ in range(len(num_word_map)):
-        # Check if the current key exists in the num_word_map
         if current in num_word_map:
-            # Add the word corresponding to the current number to the decoded message
             decoded_message += num_word_map[current] + " "
-
-        # Increment temp and update current
         temp += 1
         current += temp
 
     return decoded_message.strip()
 
-# Test the function
 file_content = decode("coding_qual_input.txt")
 print(file_content)
